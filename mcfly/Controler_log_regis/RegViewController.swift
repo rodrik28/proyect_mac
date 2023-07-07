bc91227587a3e7f0ddd07589ad27f92ad22b969a
e <e>
2023-07-07 14:00:04 -0500

    fire

--- mcfly/Controler_log_regis/RegViewController.swift
+++ mcfly/Controler_log_regis/RegViewController.swift
@@ -7,6 +7,13 @@
 
 import UIKit
 
+import FirebaseAuth
+import FirebaseCore
+
+
+import FirebaseDatabase
+
+
 class RegViewController: UIViewController {
     
     
@@ -20,6 +27,23 @@ class RegViewController: UIViewController {
 
     
     @IBAction func regi(_ sender: Any) {
+        Auth.auth().createUser(withEmail: self.usertext.text!, password: self.passtext.text!){(user,error) in
+            print("Intentando Iniciar Sesion")
+            if error != nil{
+                print("Se presento un error: \(error)")
+            }else{
+                print("El usuario fue creado exitosamente")
+                
+                Database.database().reference().child("usuarios").child(user!.user.uid).child("email").setValue(user!.user.email)
+                self.dismiss(animated: true, completion: nil)
+                
+                
+                
+               
+            }
+    }
+        
+        
         
         
         
