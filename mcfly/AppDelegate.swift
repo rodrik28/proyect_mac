dd9405b7ec7ca8899609bd8d0d1f133919ecdedf
rodrik28 <rodrijoelaronesquispe@gmail.com>
2023-07-10 06:20:33 -0500

    asdasd

--- mcfly/AppDelegate.swift
+++ mcfly/AppDelegate.swift
@@ -6,14 +6,16 @@
 //
 
 import UIKit
-import FirebaseCore
+
+// import FirebaseCore
+
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        FirebaseApp.configure()
+       // FirebaseApp.configure()
         return true
     }
 
